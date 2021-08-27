2f83ff0ba3e044bac33634d86427526b17ebcde3
Mitsunee <michelle@mitsunee.com>
2021-08-27 17:12:33 +0200

    add: Loading component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,13 @@
 import "modern-normalize/modern-normalize.css";
 import "@styles/globals.css";
 
+import { useRouterLoading } from "@utils/hooks/useRouterLoading";
+import Loading from "@components/Loading";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const loading = useRouterLoading();
+
+  return <>{loading ? <Loading /> : <Component {...pageProps} />}</>;
 }
 
 export default MyApp;
