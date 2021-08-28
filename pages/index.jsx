c010cedda369eac3a5c2f2c39a8b12aee526b560
Mitsunee <michelle@mitsunee.com>
2021-08-28 16:48:35 +0200

    add: Meta component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,13 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
+
 // import styles from '@styles/Home.module.css'
+import Meta from "@components/Meta";
 
 export default function Home() {
-  return null;
+  return (
+    <>
+      <Meta title="FGO Tools" description="BOTTOM TEXT" noTitleSuffix />
+    </>
+  );
 }
