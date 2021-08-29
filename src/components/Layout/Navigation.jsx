167f34745c08c9f7b997a30be678cef61238599c
Mitsunee <michelle@mitsunee.com>
2021-08-29 16:32:01 +0200

    feat: Implement navigation
    Still missing svg assets for header floaters tho

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -1,6 +1,41 @@
-//import styles from "./Navigation.module.css";
-//import NavigationItem from "./NavigationItem";
+import { useRouter } from "next/router";
+
+import styles from "./Navigation.module.css";
+import { navRoutes } from "@utils/navRoutes";
+import NavigationItem from "./NavigationItem";
+import NavigationSubMenu from "./NavigationSubMenu";
 
 export default function Navigation() {
-  return <nav></nav>;
+  const router = useRouter();
+  const testActive = item =>
+    item.test
+      ? item.test.test(router.asPath)
+      : router.asPath.startsWith(item.link);
+
+  return (
+    <nav className={styles.wrapper}>
+      <section className={styles.nav}>
+        {navRoutes.map(navItem => {
+          const isActiveItem = testActive(navItem);
+
+          return navItem.link instanceof Array ? (
+            <NavigationSubMenu
+              key={navItem.key || navItem.text}
+              menuKey={navItem.key}
+              text={navItem.text}
+              forceOpen={isActiveItem}
+              items={navItem.link}
+            />
+          ) : (
+            <NavigationItem
+              key={navItem.key || navItem.text}
+              link={navItem.link}
+              active={isActiveItem}>
+              {navItem.text}
+            </NavigationItem>
+          );
+        })}
+      </section>
+    </nav>
+  );
 }
