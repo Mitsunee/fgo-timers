0d70db3c716aba0e17cf16daef4db93a6129c4fd
Mitsunee <michelle@mitsunee.com>
2021-09-18 19:05:05 +0200

    add: resolveFilePath server util

--- src/utils/server/getFileList.js
+++ src/utils/server/getFileList.js
@@ -1,8 +1,10 @@
 import { join } from "path";
 import { readdir } from "fs/promises";
 
+import { resolveFilePath } from "@utils/server/resolveFilePath";
+
 export async function getFileList(dirPath) {
-  const fullPath = join(process.cwd(), dirPath);
+  const fullPath = resolveFilePath(dirPath);
   const fileList = await readdir(fullPath);
   const fileListJoined = fileList.map(ticketFile => join(fullPath, ticketFile));
 
