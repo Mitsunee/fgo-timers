1cec7f3bf212716020dbfacaed08931f31b57d00
Mitsunee <michelle@mitsunee.com>
2021-09-24 13:46:21 +0200

    refactor: EmberLootList remove memo

--- src/components/ChaldeaGateCard/EmberLootList.jsx
+++ src/components/ChaldeaGateCard/EmberLootList.jsx
@@ -1,29 +1,20 @@
-import { useMemo } from "react";
-
 //import styles from "./EmberLootList.module.css";
 import { FGOItemList, FGOItemListItem } from "@components/FGOItemList";
 
 export default function EmberLootList({ data }) {
-  const emberList = useMemo(
-    () =>
-      data.map(ember => ({
-        key: ember.class,
-        itemData: {
-          name: `${ember.class} Embers`,
-          icon: ember.ember,
-          background: "clear"
-        }
-      })),
-    [data]
-  );
-
   return (
     <>
       <h2>Daily Ember Gathering</h2>
       <FGOItemList>
-        {emberList.map(({ key, itemData }) => (
-          <FGOItemListItem key={key} data={itemData} />
-        ))}
+        {data.map(date => {
+          const itemData = {
+            name: `${date.class} Embers`,
+            icon: date.ember,
+            background: "clear"
+          };
+
+          return <FGOItemListItem key={date.class} data={itemData} />;
+        })}
       </FGOItemList>
     </>
   );
