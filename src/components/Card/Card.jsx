39c33299c73373e71c67f633b3e6cea8aa9c47ec
Mitsunee <michelle@mitsunee.com>
2021-09-25 12:44:24 +0200

    add: support for className, style and other props for Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,26 @@ import styles from "./Card.module.css";
 import CardHero from "./CardHero";
 import Headline from "@components/Headline";
 
-export default function Card({ children, icon, title, background, border }) {
+export default function Card({
+  children,
+  icon,
+  title,
+  background,
+  border,
+  className,
+  wrapperClassName,
+  style,
+  ...props
+}) {
+  const articleStyle = typeof style === "object" ? style : new Object();
+  if (border) articleStyle.borderColor = border;
+
   return (
-    <div>
+    <div className={wrapperClassName || undefined}>
       <article
-        className={styles.card}
-        style={border ? { borderColor: border } : undefined}>
+        {...props}
+        className={className ? `${styles.card} ${className}` : styles.card}
+        style={border || style ? articleStyle : undefined}>
         <CardHero icon={icon} background={background} title={title} />
         <main className={styles.inner}>
           <Headline>{title}</Headline>
