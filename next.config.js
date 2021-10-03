c308e3289236cae259e48a3815ed6212242c9040
Mitsunee <michelle@mitsunee.com>
2021-10-03 16:43:19 +0200

    install: modern-diacritics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-const transpiledModules = ["nanostores", "@nanostores/persistent"];
+const transpiledModules = [
+  "nanostores",
+  "@nanostores/persistent",
+  "modern-diacritics"
+];
 const withTM = require("next-transpile-modules")(transpiledModules); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
