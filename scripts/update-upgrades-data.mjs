44363774a918d3a001366c1d122a8627a27931e4
Mitsunee <michelle@mitsunee.com>
2021-10-08 16:16:08 +0200

    WIP: add missing interlude dates

--- scripts/update-upgrades-data.mjs
+++ scripts/update-upgrades-data.mjs
@@ -24,6 +24,134 @@ const PLACEHOLDER_SKILL = {
   icon: "https://assets.atlasacademy.io/GameData/JP/SkillIcons/skill_999999.png",
   priority: 0
 };
+const LAUNCH_GAME = 1498460400;
+const missingDatesMap = new Map([
+  [91100101, LAUNCH_GAME], // Altria Pendragon Interlude 1
+  [91100102, LAUNCH_GAME], // Altria Pendragon Interlude 2
+  [91100201, LAUNCH_GAME], // Altria Pendragon (Alter) Interlude 1
+  [91100202, LAUNCH_GAME], // Altria Pendragon (Alter) Interlude 2
+  [91100301, LAUNCH_GAME], // Altria Pendragon (Lily) Interlude 1
+  [91100501, LAUNCH_GAME], // Nero Claudius Interlude 1
+  [91100502, LAUNCH_GAME], // Nero Claudius Interlude 2
+  [91100503, LAUNCH_GAME], // Nero Claudius Interlude 3
+  // Nero Claudius (Bride) - SQ - 91100601: Bouquet of Meetings
+  // Nero Claudius (Bride) - NP - 91100602: The Bouquet of Resolve
+  [91100801, LAUNCH_GAME], // Siegfried Interlude 1
+  [91100802, LAUNCH_GAME], // Siegfried Interlude 2
+  // Mordred - NP - 91100901: King of Kings
+  [91101301, LAUNCH_GAME], // Gaius Julius Caesar Interlude 1
+  // Fergus mac Róich - NP - 91101401: Pretty Illusion
+  [91101801, LAUNCH_GAME], // Altera Interlude 1
+  [91101802, LAUNCH_GAME], // Altera Interlude 2
+  // Altera - S2 - 91101803: My Sword of the God of War
+  [91102201, LAUNCH_GAME], // Gilles de Rais Interlude 1
+  [91102601, LAUNCH_GAME], // Chevalier d'Eon Interlude 1
+  [91200101, LAUNCH_GAME], // Emiya Interlude 1
+  [91200102, LAUNCH_GAME], // Emiya Interlude 2
+  [91200201, LAUNCH_GAME], // Gilgamesh Interlude 1
+  // Gilgamesh - NP - 91200202: Law of Creation
+  // Gilgamesh - S3 - 91200203: Law of Heaven
+  [91200301, LAUNCH_GAME], // Robin Hood Interlude 1
+  // Atalante - S1 - 91200501: Smile of the Goddess
+  // Atalante - NP - 91200502: London Child
+  // Orion - SQ - 91200901: Unsweet Honeymoon
+  // Orion - NP - 91200902: A Certain Love Song
+  // Orion - SQ - 91200903: Golden Anniversary!
+  // Nikola Tesla - NP - 91201101: Lightning
+  [91201201, LAUNCH_GAME], // Euryale Interlude 1
+  // Arash - NP - 91201301: Lonely Soldier, Brave As a Lion
+  // Arjuna - NP - 91201501: True Value of the Divine Bow
+  // Gilgamesh (Child) - NP - 91201801: How to Love a Wild Flower
+  [91300101, LAUNCH_GAME], // Cú Chulainn Interlude 1
+  // Diarmuid Ua Duibhne - NP - 91300201: Twin-Spear of the Rose
+  // Karna - SQ - 91300401: Heroic Spirit of the Charity
+  [91300501, LAUNCH_GAME], // Elisabeth Báthory Interlude 1
+  [91300601, LAUNCH_GAME], // Musashibou Benkei Interlude 1
+  [91300701, LAUNCH_GAME], // Cú Chulainn (Prototype) Interlude 1
+  [91300901, LAUNCH_GAME], // Leonidas I Interlude 1
+  [91301001, LAUNCH_GAME], // Romulus Interlude 1
+  // Fionn mac Cumhaill - NP - 91301101: Fionn mac Cumhaill, The Hero who Defeated the Gods
+  // Brynhild - NP - 91301201: Yet I Want to Remain a Flame
+  // Scáthach - NP - 91301301: Kill Me If You Can
+  // Hektor - NP - 91301601: Trojan Guardian
+  // Altria Pendragon (Alter) - NP - 91301901: Unwakeable Dream, Unending War
+  // Altria Pendragon (Lancer) - SQ - 91302001: Sacred Lance King of Knights
+  [91400101, LAUNCH_GAME], // Medusa Interlude 1
+  // Francis Drake - NP - 91400301: Treasure Island
+  [91400601, LAUNCH_GAME], // Georgios Interlude 1
+  // Edward Teach - SQ - 91400801: A Man's Battle
+  // Anne Bonny & Mary Read - NP - 91400901: Capture Captain Kidd's Treasure
+  // Anne Bonny & Mary Read - SQ - 91400902: Treasure of the Knights Templar!
+  [91401101, LAUNCH_GAME], // Boudica Interlude 1
+  // Queen Medb - S2 - 91401301: Queen's Discipline
+  [91401401, LAUNCH_GAME], // Ushiwakamaru Interlude 1
+  // Alexander - NP - 91401501: The Beginning of a Trampling Conquest
+  [91401701, LAUNCH_GAME], // Marie Antoinette Interlude 1
+  [91401702, LAUNCH_GAME], // Marie Antoinette Interlude 2
+  [91401901, LAUNCH_GAME], // Martha Interlude 1
+  [91401902, LAUNCH_GAME], // Martha Interlude 2
+  // Medea - NP - 91500101: Witch's Backyard
+  [91500201, LAUNCH_GAME], // Gilles de Rais (Caster) Interlude 1
+  // Tamamo-no-Mae - NP - 91500301: E-Pal Wars
+  // Tamamo-no-Mae - S3 - 91500302: Nine-tailed Fox
+  // Nursery Rhyme - NP - 91500401: A Bunch of Books, a Crowd of Us
+  // Hans Christian Andersen - NP - 91500501: Human Observation
+  [91500701, LAUNCH_GAME], // William Shakespeare Interlude 1
+  // Leonardo da Vinci - SQ - 91500901: Not Even The Almighty Hand Can Reach Here
+  // Paracelsus von Hohenheim - NP - 91501001: Devil's Quest
+  // Charles Babbage - NP - 91501101: Iron Mobile Report C. Babbage
+  // Nitocris - NP - 91501201: Pharaoh Nitocris
+  // Mephistopheles - NP - 91501401: Tick Tock, Bang!
+  [91501501, LAUNCH_GAME], // Wolfgang Amadeus Mozart Interlude 1
+  // Medea (Lily) - NP - 91501701: Close Friend of a Witch
+  // Medea (Lily) - S2 - 91501702: Pancakes of Happiness
+  [91501901, LAUNCH_GAME][ // Waver Interlude 1
+    // Zhuge Liang (Lord El-Melloi II) - SQ - 91501902: Endless Sea
+    // Zhuge Liang (Lord El-Melloi II) - NP - 91501903: Truth of the Ascension
+    (91502101, LAUNCH_GAME)
+  ], // Cú Chulainn (Caster) Interlude 1
+  // Helena Blavatsky - NP - 91502301: The World that She Sees
+  // Thomas Edison - S3 - 91502501: Can I Do It?
+  [91600101, LAUNCH_GAME], // Sasaki Kojirou Interlude 1
+  [91600201, LAUNCH_GAME], // Hassan of the Cursed Arm Interlude 1
+  // Jack the Ripper - NP - 91600501: Jack Kills Jack
+  [91601001, LAUNCH_GAME], // Stheno Interlude 1
+  [91601002, LAUNCH_GAME], // Stheno Interlude 2
+  [91601101, LAUNCH_GAME], // Jing Ke Interlude 1
+  [91601201, LAUNCH_GAME], // Charles-Henri Sanson Interlude 1
+  [91601301, LAUNCH_GAME], // Phantom of the Opera Interlude 1
+  [91601401, LAUNCH_GAME], // Mata Hari Interlude 1
+  [91601701, LAUNCH_GAME], // Carmilla Interlude 1
+  // Mysterious Heroine X - SQ - 91601801: Saber Slayer, Chapter Dawn
+  // Mysterious Heroine X - SQ - 91601802: Saber Slayer, Chapter Battle
+  // Mysterious Heroine X - NP - 91601803: Saber Slayer, Chapter Awaken
+  // Shuten-Douji - SQ - 91602101: Drunkenness, Madness, and Rage
+  [91700101, LAUNCH_GAME], // Heracles Interlude 1
+  [91700102, LAUNCH_GAME], // Heracles Interlude 2
+  [91700201, LAUNCH_GAME], // Lancelot Interlude 1
+  [91700202, LAUNCH_GAME], // Lancelot Interlude 2
+  [91700301, LAUNCH_GAME], // Lu Bu Fengxian Interlude 1
+  // Frankenstein - NP - 91700401: The Events of a Lonely Night in November I
+  // Frankenstein - S2 - 91700402: The Events of a Lonely Night in November II
+  [91700501, LAUNCH_GAME], // Spartacus Interlude 1
+  // Sakata Kintoki - SQ - 91700601: Kintoki's Bear Extermination on Mt. Ashigara
+  // Vlad III - NP - 91700701: Legend of Dracula I
+  // Vlad III - S2 - 91700702: Legend of Dracula II
+  // Beowulf - NP - 91700801: Grendel's Second Coming
+  // Asterios - NP - 91700901: This Hand, For You
+  [91701001, LAUNCH_GAME], // Caligula Interlude 1
+  [91701101, LAUNCH_GAME], // Darius III Interlude 1
+  [91701102, LAUNCH_GAME], // Darius III Interlude 2
+  [91701301, LAUNCH_GAME], // Kiyohime Interlude 1
+  [91701501, LAUNCH_GAME], // Eric Bloodaxe Interlude 1
+  [91701601, LAUNCH_GAME], // Tamamo Cat Interlude 1
+  // Tamamo Cat - NP - 91701602: The Ring of Carnage
+  // Ibaraki-Douji - SQ - 91702201: Fire and Alcohol and Oni! Oh, My!
+  [91900101, LAUNCH_GAME], // Jeanne d'Arc Interlude 1
+  // Jeanne d'Arc - NP - 91900102: My God is with Me II
+  // Edmond Dantès - SQ - 911100201: Nightmare; or, the Call of Love and Hate
+  [] // PLACEHOLDER: prettier keeps yeeting my comma
+]);
 
 // helper functions
 const join = relPath => path.join(CWD, relPath);
@@ -120,7 +248,10 @@ const describeServant = (servantData, servantDataNA) => ({
 const describeQuest = (questData, questDataNA) => ({
   id: questData.id,
   name: questDataNA?.name || questData.name,
-  open: questDataNA?.openedAt || questData.openedAt,
+  open:
+    missingDatesMap.get(questData.id) ||
+    questDataNA?.openedAt ||
+    questData.openedAt,
   type: questData.type === "friendship" ? "interlude" : "rankup",
   unlock: {
     bond:
@@ -130,6 +261,7 @@ const describeQuest = (questData, questDataNA) => ({
       questData.releaseConditions?.find(({ type }) => type === "svtLimit")
         ?.value ?? 0,
     quest:
+      // TODO: fetch this data?
       questData.releaseConditions?.find(({ type }) => type === "questClear")
         ?.value ?? undefined
   },
