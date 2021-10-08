5d21d48cf0da3d7f5162068375f11a8913e2e1c7
Mitsunee <michelle@mitsunee.com>
2021-10-08 22:14:29 +0200

    WIP: add missing interlude dates (cont.)

--- scripts/update-upgrades-data.mjs
+++ scripts/update-upgrades-data.mjs
@@ -25,7 +25,14 @@ const PLACEHOLDER_SKILL = {
   priority: 0
 };
 const LAUNCH_GAME = 1498460400;
+const LAUNCH_OKEANOS = 1506582000;
+const LAUNCH_LONDON = 1513670400;
+const LAUNCH_AMERICA = 1521097200;
+const LAUNCH_CAMELOT = 1530169200;
 const missingDatesMap = new Map([
+  /*
+   * These quests all have Jan 1, 2000 as their open date in the game data
+   */
   [91100101, LAUNCH_GAME], // Altria Pendragon Interlude 1
   [91100102, LAUNCH_GAME], // Altria Pendragon Interlude 2
   [91100201, LAUNCH_GAME], // Altria Pendragon (Alter) Interlude 1
@@ -34,87 +41,86 @@ const missingDatesMap = new Map([
   [91100501, LAUNCH_GAME], // Nero Claudius Interlude 1
   [91100502, LAUNCH_GAME], // Nero Claudius Interlude 2
   [91100503, LAUNCH_GAME], // Nero Claudius Interlude 3
-  // Nero Claudius (Bride) - SQ - 91100601: Bouquet of Meetings
-  // Nero Claudius (Bride) - NP - 91100602: The Bouquet of Resolve
+  // Nero Claudius (Bride) - SQ - 91100601: Bouquet of Meetings (Interlude 1)
+  // Nero Claudius (Bride) - NP - 91100602: The Bouquet of Resolve (Interlude 2)
   [91100801, LAUNCH_GAME], // Siegfried Interlude 1
   [91100802, LAUNCH_GAME], // Siegfried Interlude 2
-  // Mordred - NP - 91100901: King of Kings
+  [91100901, LAUNCH_LONDON], // Mordred Interlude 1
   [91101301, LAUNCH_GAME], // Gaius Julius Caesar Interlude 1
-  // Fergus mac Róich - NP - 91101401: Pretty Illusion
+  [91101401, LAUNCH_LONDON], // Fergus mac Róich Interlude 1
   [91101801, LAUNCH_GAME], // Altera Interlude 1
   [91101802, LAUNCH_GAME], // Altera Interlude 2
-  // Altera - S2 - 91101803: My Sword of the God of War
+  // Altera - S2 - 91101803: My Sword of the God of War (Interlude 3)
   [91102201, LAUNCH_GAME], // Gilles de Rais Interlude 1
   [91102601, LAUNCH_GAME], // Chevalier d'Eon Interlude 1
   [91200101, LAUNCH_GAME], // Emiya Interlude 1
   [91200102, LAUNCH_GAME], // Emiya Interlude 2
   [91200201, LAUNCH_GAME], // Gilgamesh Interlude 1
-  // Gilgamesh - NP - 91200202: Law of Creation
-  // Gilgamesh - S3 - 91200203: Law of Heaven
+  [91200202, LAUNCH_LONDON], // Gilgamesh Interlude 2
+  // Gilgamesh - S3 - 91200203: Law of Heaven (Interlude 3)
   [91200301, LAUNCH_GAME], // Robin Hood Interlude 1
-  // Atalante - S1 - 91200501: Smile of the Goddess
-  // Atalante - NP - 91200502: London Child
-  // Orion - SQ - 91200901: Unsweet Honeymoon
-  // Orion - NP - 91200902: A Certain Love Song
-  // Orion - SQ - 91200903: Golden Anniversary!
-  // Nikola Tesla - NP - 91201101: Lightning
+  [91200501, LAUNCH_OKEANOS], // Atalante Interlude 1
+  [91200502, LAUNCH_LONDON], // Atalante Interlude 2
+  // Orion - SQ - 91200901: Unsweet Honeymoon (Interlude 1) // PLACEHOLDER: not known?
+  [91200902, LAUNCH_AMERICA], // Orion (Interlude 2)
+  // Orion - SQ - 91200903: Golden Anniversary! (Interlude 3)
+  [91201101, LAUNCH_LONDON], // Nikola Tesla Interlude 1
   [91201201, LAUNCH_GAME], // Euryale Interlude 1
-  // Arash - NP - 91201301: Lonely Soldier, Brave As a Lion
-  // Arjuna - NP - 91201501: True Value of the Divine Bow
-  // Gilgamesh (Child) - NP - 91201801: How to Love a Wild Flower
+  [91201301, LAUNCH_CAMELOT], // Arash Interlude 1
+  // Arjuna - NP - 91201501: True Value of the Divine Bow (Interlude 1)
+  // Gilgamesh (Child) - NP - 91201801: How to Love a Wild Flower (Interlude 1)
   [91300101, LAUNCH_GAME], // Cú Chulainn Interlude 1
-  // Diarmuid Ua Duibhne - NP - 91300201: Twin-Spear of the Rose
-  // Karna - SQ - 91300401: Heroic Spirit of the Charity
+  [91300201, LAUNCH_LONDON], // Diarmuid Ua Duibhne Interlude 1
+  // Karna - SQ - 91300401: Heroic Spirit of the Charity (Interlude 1)
   [91300501, LAUNCH_GAME], // Elisabeth Báthory Interlude 1
   [91300601, LAUNCH_GAME], // Musashibou Benkei Interlude 1
   [91300701, LAUNCH_GAME], // Cú Chulainn (Prototype) Interlude 1
   [91300901, LAUNCH_GAME], // Leonidas I Interlude 1
   [91301001, LAUNCH_GAME], // Romulus Interlude 1
-  // Fionn mac Cumhaill - NP - 91301101: Fionn mac Cumhaill, The Hero who Defeated the Gods
-  // Brynhild - NP - 91301201: Yet I Want to Remain a Flame
-  // Scáthach - NP - 91301301: Kill Me If You Can
-  // Hektor - NP - 91301601: Trojan Guardian
-  // Altria Pendragon (Alter) - NP - 91301901: Unwakeable Dream, Unending War
-  // Altria Pendragon (Lancer) - SQ - 91302001: Sacred Lance King of Knights
+  // Fionn mac Cumhaill - NP - 91301101: Fionn mac Cumhaill, The Hero who Defeated the Gods (Interlude 1)
+  // Brynhild - NP - 91301201: Yet I Want to Remain a Flame (Interlude 1)
+  // Scáthach - NP - 91301301: Kill Me If You Can (Interlude 1)
+  [91301601, LAUNCH_OKEANOS], // Hektor Interlude 1
+  [91301901, LAUNCH_LONDON], // Altria Pendragon (Lancer Alter) Interlude 1
+  [91302001, LAUNCH_CAMELOT], // Altria Pendragon (Lancer) Interlude 1
   [91400101, LAUNCH_GAME], // Medusa Interlude 1
-  // Francis Drake - NP - 91400301: Treasure Island
+  // Francis Drake - NP - 91400301: Treasure Island (Interlude 1)
   [91400601, LAUNCH_GAME], // Georgios Interlude 1
-  // Edward Teach - SQ - 91400801: A Man's Battle
-  // Anne Bonny & Mary Read - NP - 91400901: Capture Captain Kidd's Treasure
-  // Anne Bonny & Mary Read - SQ - 91400902: Treasure of the Knights Templar!
+  [91400801, LAUNCH_OKEANOS], // Edward Teach Interlude 1
+  [91400901, LAUNCH_OKEANOS], // Anne Bonny & Mary Read Interlude 1
+  [91400902, LAUNCH_CAMELOT], // Anne Bonny & Mary Read Interlude 2
   [91401101, LAUNCH_GAME], // Boudica Interlude 1
-  // Queen Medb - S2 - 91401301: Queen's Discipline
+  [91401301, LAUNCH_AMERICA], // Queen Medb Interlude 1
   [91401401, LAUNCH_GAME], // Ushiwakamaru Interlude 1
-  // Alexander - NP - 91401501: The Beginning of a Trampling Conquest
+  [91401501, LAUNCH_OKEANOS], // Alexander Interlude 1
   [91401701, LAUNCH_GAME], // Marie Antoinette Interlude 1
   [91401702, LAUNCH_GAME], // Marie Antoinette Interlude 2
   [91401901, LAUNCH_GAME], // Martha Interlude 1
   [91401902, LAUNCH_GAME], // Martha Interlude 2
-  // Medea - NP - 91500101: Witch's Backyard
+  [91500101, LAUNCH_OKEANOS], // Medea Interlude 1
   [91500201, LAUNCH_GAME], // Gilles de Rais (Caster) Interlude 1
-  // Tamamo-no-Mae - NP - 91500301: E-Pal Wars
-  // Tamamo-no-Mae - S3 - 91500302: Nine-tailed Fox
-  // Nursery Rhyme - NP - 91500401: A Bunch of Books, a Crowd of Us
-  // Hans Christian Andersen - NP - 91500501: Human Observation
+  // Tamamo-no-Mae - NP - 91500301: E-Pal Wars (Interlude 1) // PLACEHOLDER: not known?
+  [91500302, LAUNCH_LONDON], // Tamamo-no-Mae Interlude 2
+  [91500401, LAUNCH_LONDON], // Nursery Rhyme Interlude 1
+  [91500501, LAUNCH_LONDON], // Hans Christian Andersen Interlude 1
   [91500701, LAUNCH_GAME], // William Shakespeare Interlude 1
-  // Leonardo da Vinci - SQ - 91500901: Not Even The Almighty Hand Can Reach Here
-  // Paracelsus von Hohenheim - NP - 91501001: Devil's Quest
-  // Charles Babbage - NP - 91501101: Iron Mobile Report C. Babbage
-  // Nitocris - NP - 91501201: Pharaoh Nitocris
-  // Mephistopheles - NP - 91501401: Tick Tock, Bang!
+  // Leonardo da Vinci - SQ - 91500901: Not Even The Almighty Hand Can Reach Here (Interlude 1)
+  [91501001, LAUNCH_LONDON], // Paracelsus Interlude 1
+  [91501101, LAUNCH_LONDON], // Charles Babbage Interlude 1
+  [91501201, LAUNCH_CAMELOT], // Nitocris Interlude 1
+  [91501401, LAUNCH_LONDON], // Mephistopheles Interlude 1
   [91501501, LAUNCH_GAME], // Wolfgang Amadeus Mozart Interlude 1
-  // Medea (Lily) - NP - 91501701: Close Friend of a Witch
-  // Medea (Lily) - S2 - 91501702: Pancakes of Happiness
-  [91501901, LAUNCH_GAME][ // Waver Interlude 1
-    // Zhuge Liang (Lord El-Melloi II) - SQ - 91501902: Endless Sea
-    // Zhuge Liang (Lord El-Melloi II) - NP - 91501903: Truth of the Ascension
-    (91502101, LAUNCH_GAME)
-  ], // Cú Chulainn (Caster) Interlude 1
-  // Helena Blavatsky - NP - 91502301: The World that She Sees
-  // Thomas Edison - S3 - 91502501: Can I Do It?
+  [91501701, LAUNCH_OKEANOS], // Medea (Lily) Interlude 1
+  [91501702, LAUNCH_CAMELOT], // Medea (Lily) Interlude 2
+  [91501901, LAUNCH_GAME], // Waver Interlude 1
+  [91501902, LAUNCH_OKEANOS], // Waver Interlude 2
+  // Zhuge Liang (Lord El-Melloi II) - NP - 91501903: Truth of the Ascension (Interlude 3)
+  [91502101, LAUNCH_GAME], // Cú Chulainn (Caster) Interlude 1
+  [91502301, LAUNCH_CAMELOT], // Helena Blavatsky Interlude 1
+  [91502501, LAUNCH_AMERICA], // Thomas Edison Interlude 1
   [91600101, LAUNCH_GAME], // Sasaki Kojirou Interlude 1
   [91600201, LAUNCH_GAME], // Hassan of the Cursed Arm Interlude 1
-  // Jack the Ripper - NP - 91600501: Jack Kills Jack
+  [91600501, LAUNCH_LONDON], // Jack the Ripper Interlude 1
   [91601001, LAUNCH_GAME], // Stheno Interlude 1
   [91601002, LAUNCH_GAME], // Stheno Interlude 2
   [91601101, LAUNCH_GAME], // Jing Ke Interlude 1
@@ -125,33 +131,56 @@ const missingDatesMap = new Map([
   // Mysterious Heroine X - SQ - 91601801: Saber Slayer, Chapter Dawn
   // Mysterious Heroine X - SQ - 91601802: Saber Slayer, Chapter Battle
   // Mysterious Heroine X - NP - 91601803: Saber Slayer, Chapter Awaken
-  // Shuten-Douji - SQ - 91602101: Drunkenness, Madness, and Rage
+  // Shuten-Douji - SQ - 91602101: Drunkenness, Madness, and Rage (Interlude 1)
   [91700101, LAUNCH_GAME], // Heracles Interlude 1
   [91700102, LAUNCH_GAME], // Heracles Interlude 2
   [91700201, LAUNCH_GAME], // Lancelot Interlude 1
   [91700202, LAUNCH_GAME], // Lancelot Interlude 2
   [91700301, LAUNCH_GAME], // Lu Bu Fengxian Interlude 1
-  // Frankenstein - NP - 91700401: The Events of a Lonely Night in November I
-  // Frankenstein - S2 - 91700402: The Events of a Lonely Night in November II
+  [91700401, LAUNCH_LONDON], // Frankenstein Interlude 1
+  [91700402, LAUNCH_LONDON], // Frankenstein Interlude 2
   [91700501, LAUNCH_GAME], // Spartacus Interlude 1
-  // Sakata Kintoki - SQ - 91700601: Kintoki's Bear Extermination on Mt. Ashigara
-  // Vlad III - NP - 91700701: Legend of Dracula I
-  // Vlad III - S2 - 91700702: Legend of Dracula II
-  // Beowulf - NP - 91700801: Grendel's Second Coming
-  // Asterios - NP - 91700901: This Hand, For You
+  [91700601, LAUNCH_LONDON], // Sakata Kintoki Interlude 1
+  [91700701, LAUNCH_LONDON], // Vlad III Interlude 1
+  // Vlad III - S2 - 91700702: Legend of Dracula II (Interlude 2)
+  // Beowulf - NP - 91700801: Grendel's Second Coming (Interlude 1)
+  [91700901, LAUNCH_OKEANOS], // Asterios Interlude 1
   [91701001, LAUNCH_GAME], // Caligula Interlude 1
   [91701101, LAUNCH_GAME], // Darius III Interlude 1
   [91701102, LAUNCH_GAME], // Darius III Interlude 2
   [91701301, LAUNCH_GAME], // Kiyohime Interlude 1
   [91701501, LAUNCH_GAME], // Eric Bloodaxe Interlude 1
   [91701601, LAUNCH_GAME], // Tamamo Cat Interlude 1
-  // Tamamo Cat - NP - 91701602: The Ring of Carnage
-  // Ibaraki-Douji - SQ - 91702201: Fire and Alcohol and Oni! Oh, My!
+  [91701602, LAUNCH_LONDON], // Tamamo Cat Interlude 2
+  // Ibaraki-Douji - SQ - 91702201: Fire and Alcohol and Oni! Oh, My! (Interlude 1)
   [91900101, LAUNCH_GAME], // Jeanne d'Arc Interlude 1
-  // Jeanne d'Arc - NP - 91900102: My God is with Me II
-  // Edmond Dantès - SQ - 911100201: Nightmare; or, the Call of Love and Hate
-  [] // PLACEHOLDER: prettier keeps yeeting my comma
+  // Jeanne d'Arc - NP - 91900102: My God is with Me II (Interlude 2)
+  // Edmond Dantès - SQ - 911100201: Nightmare; or, the Call of Love and Hate (Interlude 1)
+  /*
+   * the following quest have quest.open times after Jan 1, 2000 but still before LAUNCH_GAME
+   */
+  // Minamoto-no-Raikou - SQ - 91702301: The Different Story of Ushi Gozen's Home
+  // Scáthach - SQ - 91301302: Calls from the Land of Shadows
+  // Iskandar - SQ - 91400201: Conqueror of Life
+  // Iskandar - NP - 91400202: The Unending Expedition
+  // Sakata Kintoki - SQ - 91700602: Kintoki's Oni Slaying at Mt. Ooe
+  // Amakusa Shirou - NP - 91900201: Let's Talk About Saving the World
+  /*
+   * Jekyll here at the end please, as he gets ignored currently lol
+   */
+  [91600701, LAUNCH_LONDON] // Jekyll & Hyde Interlude 1
 ]);
+/* TODO: check if these have valid timestamps:
+  - Angra (Interlude 1) released with LAUNCH_CAMELOT
+  - Iskandar (Interlude 1 and 2) releasd with LAUNCH_CAMELOT
+  - Illya (Interlude 1) released with Babylonia? (same as Ibaraki 1?)
+
+https://fategrandorder.fandom.com/wiki/User_blog:Primordialancient/Interlude_Release_Dates
+has Angra 1 and Illya 1 listed, but filtering by quest.open < LAUNCH_GAME didn't find them.
+I also don't know what "EPF" means in that doc, so I stopped there.
+There's also a table with confirmed JP dates below that, but I'd like NA dates.
+
+*/
 
 // helper functions
 const join = relPath => path.join(CWD, relPath);
