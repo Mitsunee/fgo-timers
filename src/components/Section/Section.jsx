361717fb6a44d7ffcf74bbc71578ca19e8b2129a
Mitsunee <michelle@mitsunee.com>
2021-10-12 13:39:06 +0200

    add: blue variant of section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,14 +4,17 @@ export default function Section({
   children,
   background = false,
   padding = null,
-  className = false
+  className = false,
+  ...props
 }) {
   const classNames = [styles.section];
 
   // with background enable padding unless explicitly false
   // without background enable padding if explicitly true
   if (background) {
-    classNames.push(styles.background);
+    classNames.push(
+      background === "blue" ? styles.backgroundBlue : styles.backgroundBlack
+    );
     if (padding !== false) classNames.push(styles.padding);
   } else {
     if (padding) classNames.push(styles.padding);
@@ -19,5 +22,9 @@ export default function Section({
 
   if (className) classNames.push(className);
 
-  return <section className={classNames.join(" ")}>{children}</section>;
+  return (
+    <section className={classNames.join(" ")} {...props}>
+      {children}
+    </section>
+  );
 }
