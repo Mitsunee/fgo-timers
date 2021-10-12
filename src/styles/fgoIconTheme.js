4cab1ce33c9daa503ef0876cc02cb05dbee71dfd
Mitsunee <michelle@mitsunee.com>
2021-10-12 11:46:31 +0200

    refactor FGOIcon color maps into a style import

---
+++ src/styles/fgoIconTheme.js
@@ -0,0 +1,38 @@
+const BLUE = "#9db5b5";
+export const borderColors = new Map([
+  ["black", "#000000"],
+  ["bronze", "#7d583b"],
+  ["silver", "#c1c1c1"],
+  ["gold", "#f0ce02"],
+  ["zero", BLUE],
+  ["questClearQPReward", BLUE],
+  ["blue", BLUE],
+  ["red", "#e22b52"],
+  ["green", "#1e7552"],
+  ["clear", "transparent"]
+]);
+
+const BLUE_BG = "#3a5266";
+export const backgroundColors = new Map([
+  ["black", "#000000"],
+  ["bronze", "#56361d"],
+  ["silver", "#6d6d6d"],
+  ["gold", "#f9e677"],
+  ["zero", BLUE_BG],
+  ["questClearQPReward", BLUE_BG],
+  ["blue", BLUE_BG],
+  ["red", "#a00920"],
+  ["green", "#71ca8a"],
+  ["clear", "transparent"]
+]);
+
+const BLUE_IMG = "/assets/backgrounds/material_bg_blue.png";
+export const backgrounds = new Map([
+  ["black", "/assets/backgrounds/material_bg_black.png"],
+  ["bronze", "/assets/backgrounds/material_bg_bronze.png"],
+  ["silver", "/assets/backgrounds/material_bg_silver.png"],
+  ["gold", "/assets/backgrounds/material_bg_gold.png"],
+  ["zero", BLUE_IMG],
+  ["questClearQPReward", BLUE_IMG],
+  ["blue", BLUE_IMG]
+]);
