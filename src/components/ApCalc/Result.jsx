91f42a4d8cc2d3ee60ad90e7de3c9d127d8f79ad
Mitsunee <michelle@mitsunee.com>
2021-11-22 11:31:50 +0100

    fix: ap calc using server time bug
    - useFormattedTimestamp had redundant code which has been refactored
    - added arg to ignore server time setting in useFormattedTimestamp and 
    useFormattedSpacetime
    - added new arg to ApCalc's Result component

--- src/components/ApCalc/Result.jsx
+++ src/components/ApCalc/Result.jsx
@@ -3,7 +3,7 @@ import { useFormattedTimestamp } from "@utils/hooks/useFormattedTimestamp";
 
 export default function Result({ text, time, from }) {
   const delta = useFormattedDeltaFrom(from, time);
-  const date = useFormattedTimestamp(time, "short");
+  const date = useFormattedTimestamp(time, "short", true); // ignores server time setting
 
   return (
     <p>
