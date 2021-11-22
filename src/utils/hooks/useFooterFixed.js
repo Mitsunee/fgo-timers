da2cc52606a041942fc4525357d051ea26d37dd6
Mitsunee <michelle@mitsunee.com>
2021-11-22 14:10:03 +0100

    fix: useFooterFixed not reacting to content changes

--- src/utils/hooks/useFooterFixed.js
+++ src/utils/hooks/useFooterFixed.js
@@ -1,21 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useStore } from "@nanostores/react";
 
-import { useIsClient } from "@utils/hooks/useIsClient";
 import { clientStore } from "@stores/clientStore";
 
-export const useFooterFixed = () => {
-  const [footerFixed, setFooterFixed] = useState(false);
-  const { height } = useStore(clientStore);
-  const isClient = useIsClient();
+function makeObserver(fn) {
+  try {
+    return new ResizeObserver(fn);
+  } catch {
+    return null;
+  }
+}
 
-  /* eslint-disable react-hooks/exhaustive-deps */
+export const useFooterFixed = () => {
+  const [footerFixed, setFooterFixed] = useState(true);
+  const [nextHeight, setNextHeight] = useState(0);
+  const { height: windowHeight } = useStore(clientStore);
+  const observerRef = useRef(
+    makeObserver(([entry]) => setNextHeight(entry.contentRect.height))
+  );
+
+  // mount observer
   useEffect(() => {
-    if (isClient) {
-      const [el] = document.getElementById("__next").getClientRects();
-      setFooterFixed(el.height < height);
-    }
-  });
+    const { current: observer } = observerRef;
+    if (observer.current === null || typeof document === "undefined") return;
+
+    observer.observe(document.querySelector("#__next"));
+
+    // Remove the observer as soon as the component is unmounted
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    setFooterFixed(nextHeight <= windowHeight);
+  }, [nextHeight, windowHeight]);
 
   return footerFixed;
 };
