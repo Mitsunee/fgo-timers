5f97484a746d4cdd52392983e3125b648795b1e0
Mitsunee <michelle@mitsunee.com>
2021-12-08 08:49:55 +0100

    add: exchange-tickets route to navRoutes

--- src/utils/navRoutes.js
+++ src/utils/navRoutes.js
@@ -1,5 +1,14 @@
+import spacetime from "spacetime";
+
+const currentYear = spacetime.now().year();
+
 export const navRoutes = [
   { link: "/", text: "Timers", test: /^\/$/ },
+  {
+    link: `/exchange-tickets/${currentYear}`,
+    text: "Login Exchange Tickets",
+    test: /^\/exchange-tickets\/\d+\/?$/
+  },
   { link: "/upgrades", text: "Upgrades" },
   { link: "/ap-calc", text: "AP Calculator" }
 ];
