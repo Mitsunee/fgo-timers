dbdce93a189952711e905b973da25e195e1440f7
Mitsunee <michelle@mitsunee.com>
2021-12-13 09:13:47 +0100

    Update 2021-12-13 (#45)
    Updates:
    - Event data
    
    Changes:
    - Reverse event list order
    - hide LB5 release
    - Redirect xmas-2021-temp to xmas-2021

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 const config = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/events/xmas-2021-temp",
+        destination: "/events/xmas-2021",
+        permanent: false
+      }
+    ];
+  }
 };
 
 export default config;
