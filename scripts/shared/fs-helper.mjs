a5574c779bca3535e99a0069171f9a999065bd87
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2021-12-25 15:51:27 +0100

    add: directory sanity checks to fs-helper script util

--- scripts/shared/fs-helper.mjs
+++ scripts/shared/fs-helper.mjs
@@ -1,29 +1,71 @@
 import fs from "fs/promises";
-
+import { dirname } from "path";
 import { resolveFilePath } from "./path-helper.mjs";
 
-export async function readFile(path) {
-  const realpath = resolveFilePath(path);
-  return await fs.readFile(realpath, "utf8");
-}
-
-export async function readFileJson(path) {
-  const realpath = resolveFilePath(path);
-  const content = await fs.readFile(realpath, "utf8");
+export async function dirExists(filePath) {
+  const fullPath = resolveFilePath(filePath);
   try {
-    return JSON.parse(content);
-  } catch (e) {
-    return content;
+    const dirStat = await fs.stat(fullPath);
+    return dirStat.isDirectory();
+  } catch {
+    return false;
   }
 }
 
-export async function writeFile(path, data, pretty) {
-  const realpath = resolveFilePath(path);
-  const content =
-    typeof data !== "string"
-      ? pretty
-        ? JSON.stringify(data, null, 2)
-        : JSON.stringify(data)
-      : data;
-  await fs.writeFile(realpath, content, "utf8");
+export async function fileExists(filePath) {
+  const fullPath = resolveFilePath(filePath);
+  try {
+    const fileStat = await fs.stat(fullPath);
+    return fileStat.isFile();
+  } catch {
+    return false;
+  }
+}
+
+export async function makeDir(dirPath) {
+  await fs.mkdir(resolveFilePath(dirPath), { recursive: true });
+}
+
+export async function writeFile(filePath, content) {
+  if (content === undefined || content === null) return false;
+
+  // prep directory
+  const dirPath = dirname(filePath);
+  if (!(await dirExists(dirPath))) {
+    await makeDir(dirPath);
+  }
+
+  // write File
+  const fullPath = resolveFilePath(filePath);
+  if (typeof content === "object") {
+    // write objects as stringified json
+    await fs.writeFile(fullPath, JSON.stringify(content), "utf8");
+    return;
+  }
+
+  await fs.writeFile(fullPath, `${content}`, "utf8");
+}
+
+export async function readFile(filePath) {
+  // check that file exists
+  if (!fileExists(filePath)) {
+    return false;
+  }
+
+  // read file
+  try {
+    return await fs.readFile(resolveFilePath(filePath), "utf8");
+  } catch {
+    return false;
+  }
+}
+
+export async function readFileJson(filePath) {
+  try {
+    const fileContent = await readFile(filePath);
+    if (!fileContent) return false;
+    return JSON.parse(fileContent);
+  } catch {
+    return false;
+  }
 }
