f83cc483980e78674ec5ec915c25c29e9871a2b7
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2021-12-28 11:02:12 +0100

    add: EnmaTeiTimer component

---
+++ src/components/EnmaTeiTimer/EnmaTeiTimer.jsx
@@ -0,0 +1,23 @@
+import { isClamped } from "foxkit/clamp";
+
+import styles from "./EnmaTeiTimer.module.css";
+import { useRecurringDaily } from "@utils/hooks/useRecurringDaily";
+import { useFormattedTimestamp } from "@utils/hooks/useFormattedTimestamp";
+import { useFormattedDelta } from "@utils/hooks/useFormattedDelta";
+import Section from "@components/Section";
+import InlineIcon from "@components/InlineIcon";
+
+export default function EnmaTeiTimer({ endsAt, startsAt }) {
+  const nextPayout = useRecurringDaily({ hour: 10 });
+  const date = useFormattedTimestamp(nextPayout, "short");
+  const delta = useFormattedDelta(nextPayout);
+
+  return isClamped({ value: nextPayout, max: endsAt, min: startsAt }) ? (
+    <Section className={styles.section} background>
+      <span>
+        Next Payout in: {delta} ({date})
+      </span>
+      <InlineIcon className={styles.icon} icon="/assets/icon_enmapoints.png" />
+    </Section>
+  ) : null;
+}
