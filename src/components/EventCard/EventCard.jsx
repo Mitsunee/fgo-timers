aad0edc5fdbc88572da2ee75e1988f29898780fc
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-01-30 12:29:14 +0100

    Revert "fix: potential fix for frontpage desync"
    This reverts commit 24d53d7ab0db09b5b9ab208215e05a9fde65d8cf.

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 import { useStore } from "@nanostores/react";
 
-//import styles from "./EventCard.module.css";
+import styles from "./EventCard.module.css";
 import { intervalStore } from "@stores/intervalStore";
-import Container from "./Container";
 import Overlay from "./Overlay";
 import NoSSR from "@components/NoSSR";
 import TimeDisplay from "./TimeDisplay";
@@ -24,17 +23,14 @@ export default function EventCard({
   }
 
   return (
-    <Link href={`/events/${slug}/`} passHref key={shortTitle}>
-      <Container
-        title={title}
-        shortTitle={shortTitle}
-        banner={banner}
-        slug={slug}>
+    <Link href={`/events/${slug}/`} passHref>
+      <a className={styles.card} title={title}>
+        <img src={`/assets/events/${banner}`} alt={slug} />
         <Overlay title={title} shortTitle={shortTitle} />
         <NoSSR>
           <TimeDisplay startsAt={startsAt} endsAt={endsAt} />
         </NoSSR>
-      </Container>
+      </a>
     </Link>
   );
 }
