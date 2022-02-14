52ff78d928e656708ab86961e0c148f4adf9e1eb
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-02-14 09:42:13 +0100

    Fix Limited Prism Shop
    Fixes:
    - Limited Prism Shop not displaying correctly
    
    Updates:
    - Mana Prism Shop Data
    - 16M Downloads Campaign

--- src/components/ShopCard/ShopCardLimitedItem.jsx
+++ src/components/ShopCard/ShopCardLimitedItem.jsx
@@ -6,24 +6,20 @@ import InlineIcon from "@components/InlineIcon";
 
 export default function ShopCardLimitedItem({ currency, data }) {
   const { cost, amount, stack, ends, icon, ...itemData } = data;
+  const listData = {
+    ...itemData,
+    icon: `https://static.atlasacademy.io/${icon}`
+  };
   const { name } = itemData;
   const isClient = useIsClient();
   const endDate = useFormattedTimestamp(ends * 1000, "short");
 
   return (
-    <FGOItemListItem
-      data={itemData}
-      icon={`https://static.atlasacademy.io/${icon}`}>
+    <FGOItemListItem data={listData}>
       {stack && `${isClient ? stack.toLocaleString() : stack}x `}
-      {name}
-      {amount && cost && (
-        <>
-          {`(${amount}${stack ? ` stacks` : `x`}, ${
-            isClient ? cost.toLocaleString() : cost
-          }) `}
-          <InlineIcon icon={currency} /> each)
-        </>
-      )}
+      {name} ({amount}
+      {stack ? ` stacks` : `x`}, {isClient ? cost.toLocaleString() : cost}{" "}
+      <InlineIcon icon={currency} /> each)
       <br />
       {isClient && <>Available until {endDate}</>}
     </FGOItemListItem>
