8535cd15f4053a13c762e40783b8abd4c7102047
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-03-22 13:49:25 +0100

    add: atlas api server util

---
+++ src/server/utils/atlasacademy.js
@@ -0,0 +1,16 @@
+import { createApi } from "picoapi";
+
+// TODO: add default headers when that feature is added to picoapi
+
+const basePath = "https://api.atlasacademy.io";
+
+// nice endpoints
+export const atlasNa = createApi(`${basePath}/nice/NA`);
+export const atlasJp = createApi(`${basePath}/nice/JP`);
+
+// basic endpoints
+export const atlasBasicNa = createApi(`${basePath}/basic/NA`);
+export const atlasBasicJp = createApi(`${basePath}/basic/JP`);
+
+// exports endpoints
+export const atlasExport = createApi(`${basePath}/export`);
