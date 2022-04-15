fb3c4fc8237bf3c9a43a6e467e75250f70f1f029
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-04-15 13:44:32 +0200

    install and configure jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,16 @@
 const restrictedGlobals = require("confusing-browser-globals");
 
 module.exports = {
+  parserOptions: { sourceType: "module" },
   extends: ["eslint:recommended", "foxkit", "foxkit/react", "next", "prettier"],
   rules: {
     "no-restricted-globals": ["error"].concat(restrictedGlobals),
     "@next/next/no-img-element": "off"
-  }
+  },
+  overrides: [
+    {
+      files: ["tests/server/**/*.test.js", "tests/client/**/*.test.js?(x)"],
+      env: { jest: true }
+    }
+  ]
 };
