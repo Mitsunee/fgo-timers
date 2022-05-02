d7254fc8b1ff278392054dd2454ec1b800e621d5
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-05-02 09:24:09 +0200

    add: redirect from events/17m-downloads-campaign to events/17m-ssr-ticket

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@ const config = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
+  },
+  async redirects() {
+    return [
+      {
+        source: "/events/17m-downloads-campaign",
+        destination: "/events/17m-ssr-ticket",
+        permanent: false
+      }
+    ];
   }
 };
 
