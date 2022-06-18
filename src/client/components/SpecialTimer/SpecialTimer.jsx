7117aa41cd3b1aac4e016a18873c8b62a379c5eb
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-06-18 15:47:48 +0200

    add: Anime Expo Panel Timer

--- src/client/components/SpecialTimer/SpecialTimer.jsx
+++ src/client/components/SpecialTimer/SpecialTimer.jsx
@@ -1,23 +1,26 @@
-import { isClamped } from "foxkit/clamp";
+//import { isClamped } from "foxkit/clamp";
 
 import styles from "./SpecialTimer.module.css";
-import { useRecurringDaily } from "@utils/hooks/useRecurringDaily";
+//import { useRecurringDaily } from "@utils/hooks/useRecurringDaily";
 import { useFormattedTimestamp } from "@utils/hooks/useFormattedTimestamp";
 import { useFormattedDelta } from "@utils/hooks/useFormattedDelta";
 import Section from "@components/Section";
 import InlineIcon from "@components/InlineIcon";
 
-export default function SpecialTimer({ endsAt, startsAt }) {
-  const nextPayout = useRecurringDaily({ hour: 10 });
-  const date = useFormattedTimestamp(nextPayout, "short");
-  const delta = useFormattedDelta(nextPayout);
+export default function SpecialTimer(/*{ endsAt, startsAt }*/) {
+  // const nextPayout = useRecurringDaily({ hour: 10 });
+  const tz = 1656885600000;
+  const date = useFormattedTimestamp(tz, "short");
+  const delta = useFormattedDelta(tz);
 
-  return isClamped({ value: nextPayout, max: endsAt, min: startsAt }) ? (
-    <Section className={styles.section} background>
+  return (
+    /*isClamped({ value: tz, max: endsAt, min: startsAt }) ?*/ <Section
+      className={styles.section}
+      background>
       <span>
-        Next Payout in: {delta} ({date})
+        Anime Expo Panel: {delta} ({date})
       </span>
-      <InlineIcon className={styles.icon} icon="/assets/icon_enmapoints.png" />
+      <InlineIcon className={styles.icon} icon="/assets/icon_anni5.png" />
     </Section>
-  ) : null;
+  ); /* : null;*/
 }
