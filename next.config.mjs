3981d2f29b5ce055267f738553a3f760b74a7c81
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-06-28 19:24:46 +0200

    migrate to Next.js 12.2 and React 18

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 const config = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  async redirects() {
-    return [
-      {
-        source: "/events/17m-downloads-campaign",
-        destination: "/events/17m-ssr-ticket",
-        permanent: false
-      }
-    ];
-  }
+  browsersListForSwc: true,
+  eslint: { ignoreDuringBuilds: true }
 };
 
 export default config;
