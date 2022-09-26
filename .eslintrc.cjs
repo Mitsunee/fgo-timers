429b382f72304c34db777ef351f7ee4404888312
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-09-26 12:17:54 +0200

    add: setup typescript + workflows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,14 @@ const restrictedGlobals = require("confusing-browser-globals");
 
 module.exports = {
   parserOptions: { sourceType: "module" },
-  extends: ["eslint:recommended", "foxkit", "foxkit/react", "next", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "foxkit",
+    "foxkit/react",
+    "foxkit/ts",
+    "next",
+    "prettier"
+  ],
   rules: {
     "no-restricted-globals": ["error"].concat(restrictedGlobals),
     "@next/next/no-img-element": "off"
