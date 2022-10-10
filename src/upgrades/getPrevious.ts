b8bd3d9e84404234735c447128f9183cd1bea334
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-10-10 12:03:43 +0200

    add: getPrevious upgrades utils

---
+++ src/upgrades/getPrevious.ts
@@ -0,0 +1,39 @@
+import { NoblePhantasm } from "@atlasacademy/api-connector/dist/Schema/NoblePhantasm";
+import { Servant } from "@atlasacademy/api-connector/dist/Schema/Servant";
+import { Skill } from "@atlasacademy/api-connector/dist/Schema/Skill";
+import { decensorEoRNP } from "./decensorEoRNP";
+
+export function getPreviousSkill(
+  servant: Servant,
+  subject: Skill
+): Skill | undefined {
+  return servant.skills
+    .filter(skill => skill.num == subject.num && skill.id < subject.id)
+    .sort((a, b) => b.id - a.id)
+    .at(0);
+}
+
+export function getPreviousNP(
+  servant: Servant,
+  subject: NoblePhantasm
+): NoblePhantasm {
+  const nps = servant.noblePhantasms
+    .filter(np => np.priority > 0 && np.id < subject.id)
+    .sort((a, b) => b.id - a.id);
+
+  if (!nps.length) {
+    throw new Error(
+      `Could not find previous NP for id ${subject.id} for servant [${servant.id}] ${servant.name}`
+    );
+  }
+
+  // find censored EoR NP if possible. This is needed as other utils will
+  // find that NP's id and we want to use the same one to avoid duplicated
+  // NPs in the cache and build output
+  const { strengthStatus } = nps[0];
+  const eor = nps.find(
+    np => np.strengthStatus == strengthStatus && np.name == "???"
+  );
+
+  return eor ? decensorEoRNP(eor, servant) : nps[0];
+}
---
+++ tests/upgrades/getPrevious.test.ts
@@ -0,0 +1,95 @@
+import type { Servant } from "@atlasacademy/api-connector/dist/Schema/Servant";
+import { atlasCache } from "src/atlas-api/cache";
+import { getPreviousSkill, getPreviousNP } from "src/upgrades/getPrevious";
+
+let servants: Servant[];
+
+beforeAll(async () => {
+  const [niceServant, niceServantNA] = await Promise.all([
+    atlasCache.JP.getNiceServant(),
+    atlasCache.NA.getNiceServant()
+  ]);
+  const res = [
+    niceServantNA.find(servant => servant.id == 103000), // 0 - Suzuka Gozen - standard servant
+    niceServantNA.find(servant => servant.id == 202100), // 1 - Tomoe Gozen - EoR Servant
+    niceServantNA.find(servant => servant.id == 200100), // 2 - Emiya Archer - double skill upgrade & multi-type upgraded NP
+    niceServant.find(servant => servant.id == 100800), // 3 - Siegfried (Golden Rule upgrade not yet released on NA)
+    niceServant.find(servant => servant.id == 700700), // 4 - 5* Vlad - double NP upgrade
+    niceServantNA.find(servant => servant.id == 100500) // 5 - Nero 4* Saber - skill unlocked by upgrade
+  ];
+  if (res.some(servant => !servant)) throw new Error("Invalid test data");
+  servants = res as Servant[];
+});
+
+describe("getPreviousSkill", () => {
+  it("handles standard servant", () => {
+    const suzuka = servants[0];
+    expect(
+      getPreviousSkill(suzuka, suzuka.skills.find(skill => skill.id == 787451)!)
+    ).toEqual(suzuka.skills.find(skill => skill.id == 359450));
+    expect(
+      getPreviousSkill(suzuka, suzuka.skills.find(skill => skill.id == 93451)!)
+    ).toBe(undefined);
+  });
+  it("handles edgecase: double skill upgrade", () => {
+    const emiya = servants[2];
+    const skill = emiya.skills
+      .filter(skill => skill.num == 3)
+      .sort((a, b) => a.id - b.id); // sorted by upgrade level where 0 is no upgrade, 2 is both
+    expect(getPreviousSkill(emiya, skill[2])).toEqual(skill[1]);
+    expect(getPreviousSkill(emiya, skill[1])).toEqual(skill[0]);
+    expect(getPreviousSkill(emiya, skill[0])).toBe(undefined);
+  });
+  it("handles edgecase: improperly enumerated skill", () => {
+    const siegfried = servants[3];
+    expect(
+      getPreviousSkill(
+        siegfried,
+        siegfried.skills.find(skill => skill.id == 2077550)!
+      )
+    ).toEqual(siegfried.skills.find(skill => skill.id == 97349));
+  });
+  it("handles edgecase: 3rd skill unlocked by upgrade", () => {
+    const nero = servants[5];
+    expect(
+      getPreviousSkill(nero, nero.skills.find(skill => skill.id == 154550)!)
+    ).toBe(undefined);
+  });
+});
+
+describe("getPreviousNP", () => {
+  it("handles standard servant", () => {
+    const suzuka = servants[0];
+    const noup = suzuka.noblePhantasms.find(np => np.id == 103001)!;
+    expect(
+      getPreviousNP(suzuka, suzuka.noblePhantasms.find(np => np.id == 103002)!)
+    ).toEqual(noup);
+    expect(() => getPreviousNP(suzuka, noup)).toThrowError(
+      /Could not find previous/
+    );
+  });
+  it("handles edgecase: EoR NP", () => {
+    const tomoe = servants[1];
+    const result = getPreviousNP(
+      tomoe,
+      tomoe.noblePhantasms.find(np => np.id == 202102)!
+    );
+    expect(result.id).toBe(202100);
+    expect(result.name).toBe("On Arorikya Sowaka");
+  });
+  it("handles edgecase: multi-type np", () => {
+    const emiya = servants[2];
+    expect(
+      getPreviousNP(emiya, emiya.noblePhantasms.find(np => np.id == 200102)!)
+    ).toEqual(emiya.noblePhantasms.find(np => np.id == 200101));
+  });
+  it("handles edgecase: double np upgrade", () => {
+    const vlad = servants[4];
+    const nps = vlad.noblePhantasms.sort((a, b) => a.id - b.id); // sorted by upgrade level where 0 is no upgrade, 2 is both
+    expect(getPreviousNP(vlad, nps[2])).toEqual(nps[1]);
+    expect(getPreviousNP(vlad, nps[1])).toEqual(nps[0]);
+    expect(() => getPreviousNP(vlad, nps[0])).toThrow(
+      /Could not find previous/
+    );
+  });
+});
