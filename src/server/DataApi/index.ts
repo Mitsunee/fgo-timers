b2de0a7bac24e4d89d732cdb58747e605ef20f3e
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-11-03 16:03:03 +0100

    fix: add missing file from previous commit

---
+++ src/server/DataApi/index.ts
@@ -0,0 +1,34 @@
+import { BundledNP, BundledServant, BundledSkill } from "src/servants/types";
+import { BundledQuest } from "src/upgrades/types";
+
+export interface DataSets {
+  servants: IDMap<BundledServant>;
+  quests: IDMap<BundledQuest>;
+  skills: IDMap<BundledSkill>;
+  nps: IDMap<BundledNP>;
+}
+
+export type ResponseError = { data?: undefined; success: false; error: string };
+
+export interface ResponseData<Keys extends keyof DataSets = any> {
+  success: true;
+  data: Pick<DataSets, Keys>;
+}
+
+export type DataApiResponse<Keys extends keyof DataSets = any> =
+  | ResponseError
+  | ResponseData<Keys>;
+
+export const fetcher = async (url: URL) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    try {
+      const { error } = await res.json();
+      throw new Error(error);
+    } catch {
+      throw new Error("Unexpected Error");
+    }
+  }
+
+  return res.json();
+};
