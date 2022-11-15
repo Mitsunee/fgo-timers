403346e3eb48786e09637fc234f239aa9127f640
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-11-15 16:27:57 +0100

    add: implement servant availability in prebuild

--- src/prebuild/bundleServantsData.ts
+++ src/prebuild/bundleServantsData.ts
@@ -1,20 +1,40 @@
 import { List } from "@foxkit/util/object";
 import { EntityType } from "@atlasacademy/api-connector/dist/Schema/Entity.js";
+import { join } from "path";
+import { readFileYaml } from "@foxkit/node-util/fs-yaml";
 import { Log } from "../utils/log";
-import type { BundledServant } from "../servants/types";
+import { BundledServant, ServantAvailability } from "../servants/types";
 import { nameServant } from "../servants/nameServant";
 import { mapServantRarityToBorder } from "../servants/borders";
 import { atlasCache } from "../atlas-api/cache";
 import { shortenAtlasUrl } from "../atlas-api/urls";
+import {
+  ServantAvailabilityMap,
+  ServantAvailabilitySchema
+} from "../schema/ServantAvailabilityMap";
+import { verifySchema } from "../schema/verifySchema";
 import type { DataBundler } from "./dataBundlers";
+
+async function getAvailabilityMap() {
+  const filePath = join("assets", "data", "servants", "availability.yml");
+  const data = await readFileYaml<Partial<ServantAvailabilityMap>>(filePath);
+  if (!data || !verifySchema(data, ServantAvailabilitySchema, filePath)) {
+    return false;
+  }
+  return data;
+}
 
 export const bundleServantsData: DataBundler<
   BundledServant
 > = async bundles => {
-  const [basicServant, basicServantNA] = await Promise.all([
+  const [basicServant, basicServantNA, availabilityMap] = await Promise.all([
     atlasCache.JP.getBasicServant(),
-    atlasCache.NA.getBasicServant()
+    atlasCache.NA.getBasicServant(),
+    getAvailabilityMap()
   ]);
+
+  if (!availabilityMap) return false;
+
   const servantQueue = new List<number>(); // to be processed
   const knownServants = new Set<number>(); // are queued or processed
   const res = new Map<number, BundledServant>(); // result of processing
@@ -48,7 +68,20 @@ export const bundleServantsData: DataBundler<
     };
 
     if (servantNA) data.na = true;
-    // TODO: implement availability lookup
+
+    if (availabilityMap.storylocked.includes(servant.id)) {
+      data.availability = ServantAvailability.STORYLOCKED;
+    } else if (availabilityMap.limited.includes(servant.id)) {
+      data.availability = ServantAvailability.LIMITED;
+    } else if (availabilityMap.welfare.includes(servant.id)) {
+      data.availability = ServantAvailability.WELFARE;
+    } else if (availabilityMap.fp_pool.includes(servant.id)) {
+      data.availability = ServantAvailability.FP_POOL;
+    } else if (availabilityMap.fp_limited.includes(servant.id)) {
+      data.availability = ServantAvailability.FP_LIMITED;
+    } else if (availabilityMap.fp_storylocked.includes(servant.id)) {
+      data.availability = ServantAvailability.FP_LOCKED;
+    }
 
     res.set(servantId, data);
   }
