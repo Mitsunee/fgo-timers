dda5d30d10f6f67e2af5c80a7a16a2fc8b0fa511
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-12-26 09:40:01 +0100

    fix: UpgradeCard not using IconFace
    Also fixed a background-color issue causing spoilered names to be readable
    
    Closes #85

--- src/pages/UpgradesPage/components/Hero.tsx
+++ src/pages/UpgradesPage/components/Hero.tsx
@@ -1,7 +1,6 @@
 import { BgColours, Borders } from "src/types/borders";
-import { useSpoilerState } from "src/client/utils/hooks/useSpoilerState";
-import { expandAtlasUrl } from "src/atlas-api/urls";
 import { BundledServant } from "src/servants/types";
+import { IconFace } from "src/client/components/BorderedIcon";
 import styles from "./Hero.module.css";
 
 type HeroProps = {
@@ -12,17 +11,21 @@ type HeroProps = {
 export function Hero({ border, id, name, icon, na }: HeroProps) {
   const bg = BgColours[border];
   const vars = { "--bg": bg } as React.CSSProperties;
-  const [hidden] = useSpoilerState(id);
 
   return (
     <header className={styles.hero} style={vars}>
-      <div className={styles.icon}>
-        <img
-          src={expandAtlasUrl(icon)}
-          alt={!na && hidden ? "Servant" : name}
-          width="76"
-          height="76"
-        />
+      <div className={styles.iconWrapper}>
+        <div className={styles.icon}>
+          <IconFace
+            id={id}
+            name={name}
+            na={na}
+            src={icon}
+            placeholder="Servant"
+            width="76"
+            height="76"
+          />
+        </div>
       </div>
     </header>
   );
