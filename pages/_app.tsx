c1193818477fc3f7a4f4d39f59f1f651333de47c
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-01-11 14:17:46 +0100

    add: setup tRPC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppType } from "next/app";
 import "modern-normalize/modern-normalize.css";
 import "src/client/styles/app.css";
 import { Layout } from "src/client/components/Layout";
+import { api } from "src/client/api";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App: AppType = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-}
+};
+
+export default api.withTRPC(App);
