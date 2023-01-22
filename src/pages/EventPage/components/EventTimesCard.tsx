bc75c32f4c86f1a8d8de4a9a9db48d55c381e7ea
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-01-22 11:44:28 +0100

    finish: EventTimesCard

--- src/pages/EventPage/components/EventTimesCard.tsx
+++ src/pages/EventPage/components/EventTimesCard.tsx
@@ -1,13 +1,15 @@
-import spacetime from "spacetime";
+import cc from "classcat";
 import { useStore } from "@nanostores/react";
 import type { BundledEvent } from "src/events/types";
-import { formatDiff } from "src/utils/formatDiff";
 import { useIsClient } from "src/client/utils/hooks/useIsClient";
 import { intervalStore } from "src/client/stores/intervalStore";
 import { Card } from "src/client/components/Card";
+import { DisplayDate, DisplayDelta } from "src/client/components/TimeDisplay";
+import {
+  BorderedCEIcon,
+  BorderedServantIcon
+} from "src/client/components/BorderedIcon";
 import type { EventPageProps } from "../getStaticProps";
-import { DisplayDate } from "@components/TimeDisplay";
-import { BorderedCEIcon, BorderedServantIcon } from "@components/BorderedIcon";
 import styles from "./EventTimesCard.module.css";
 
 type EventTime = Exclude<BundledEvent["times"], undefined>;
@@ -28,80 +30,81 @@ interface EventTimesCardProps extends WithMaps {
 function EventListItem({ time, servants, ces }: EventListItemProps) {
   const isClient = useIsClient();
   const [start, end = 0] = Array.isArray(time.date) ? time.date : [time.date];
-  const { seconds: current, s } = useStore(intervalStore);
+  const { seconds: current } = useStore(intervalStore);
   const hasStarted = current >= start;
   const hasEnded = end > 0 ? current >= end : hasStarted;
-  const delta = formatDiff(
-    s.diff(spacetime((hasStarted ? end : start) * 1000))
-  );
 
   return (
-    <>
-      {time.title}
-      <ul className={styles.list}>
+    <li className={cc([styles.details, hasEnded && styles.ended])}>
+      <h2>{time.title}</h2>
+      <ul>
         {isClient ? (
           <>
-            <li className={styles.wide}>
-              {hasEnded ? (
-                "Has Ended"
-              ) : (
-                <>
-                  <b>{hasStarted ? "End" : "Start"}s in:</b> {delta}
-                </>
-              )}
-            </li>
+            {end > 0 && hasEnded && <li className={styles.wide}>Has Ended</li>}
             <li>
               <b>Start{hasStarted ? "ed" : "s"}:</b>{" "}
-              <DisplayDate time={start} />
+              <DisplayDate time={start * 1000} />
             </li>
             {end > 0 && (
               <li>
-                <b>End{hasEnded ? "ed" : "s"}:</b> <DisplayDate time={end} />
+                <b>End{hasEnded ? "ed" : "s"}:</b>{" "}
+                <DisplayDate time={end * 1000} />
+              </li>
+            )}
+            {!hasEnded && (
+              <li className={styles.wide}>
+                <b>
+                  {end > 0 ? `${hasStarted ? "End" : "Start"}s in:` : "In:"}
+                </b>{" "}
+                <DisplayDelta time={(hasStarted ? end : start) * 1000} />
               </li>
             )}
           </>
         ) : (
           <>
             <li>
-              Starts: <DisplayDate time={start} />
+              Starts: <DisplayDate time={start * 1000} />
             </li>
             {end > 0 && (
               <li>
-                Ends: <DisplayDate time={end} />
+                Ends: <DisplayDate time={end * 1000} />
               </li>
             )}
           </>
         )}
         {(time.servants || time.ces) && (
-          // TODO: disableSpoilers props on BorderedServantIcon and BorderedCEIcon
           <li className={styles.wide}>
-            <div className={styles.servants}>
+            <div className={styles.entities}>
               {time.servants?.map(id => (
                 <BorderedServantIcon
                   key={id}
                   servantId={id}
                   {...servants[id]}
+                  disableSpoilers
                 />
               ))}
               {time.ces?.map(id => (
-                <BorderedCEIcon key={id} ceId={id} {...ces[id]} />
+                <BorderedCEIcon
+                  key={id}
+                  ceId={id}
+                  {...ces[id]}
+                  disableSpoilers
+                />
               ))}
             </div>
           </li>
         )}
       </ul>
-    </>
+    </li>
   );
 }
 
 export function EventTimesCard({ times, servants, ces }: EventTimesCardProps) {
   return (
     <Card icon="/assets/icon_times.png" title="Timers" bypassSpoilers>
-      <ul>
+      <ul className={styles.list}>
         {times.map((time, i) => (
-          <li key={i}>
-            <EventListItem time={time} servants={servants} ces={ces} />
-          </li>
+          <EventListItem key={i} time={time} servants={servants} ces={ces} />
         ))}
       </ul>
     </Card>
