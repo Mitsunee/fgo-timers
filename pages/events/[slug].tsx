d0fd10e29faba5e21db8481fca3c534f6bce4655
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-01-24 11:45:32 +0100

    refactor: add EventPageLayout

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
-import Meta from "src/client/components/Meta";
-import { Clocks } from "src/client/components/Clocks";
 import { ActionButton } from "src/client/components/Button";
 import type { EventPageProps } from "src/pages/EventPage/getStaticProps";
 import { RelatedUpgrades } from "src/pages/EventPage/components/RelatedUpgrades";
-import { EventHero } from "src/pages/EventPage/components/EventHero";
-import { EventInfoSection } from "src/pages/EventPage/components/EventInfoSection";
-import { EventNewsModal } from "src/pages/EventPage/components/EventNewsModal";
 import { CardGrid } from "@components/Card";
 import { EventTimesCard } from "src/pages/EventPage/components/EventTimesCard";
 import { EventSchedulesCard } from "src/pages/EventPage/components/EventSchedulesCard";
-import styles from "src/pages/EventPage/EventPage.module.css";
 import { EventBannerCard } from "src/pages/EventPage/components/EventBannerCard";
+import { EventPageLayout } from "src/pages/EventPage/components/EventPageLayout";
+import styles from "src/pages/EventPage/EventPage.module.css";
 
 // Next Page configs
 export {
@@ -26,34 +22,12 @@ export const config = {
   ]
 };
 
-// TODO: maybe more info in info section such as amount of banners or upgrades?
 export default function EventPage({ event, servants, ces }: EventPageProps) {
-  const metaDesc = event.description.split("\n")[0];
   const [showUpgrades, setShowUpgrades] = useState(false);
-  const [showEmbed, setShowEmbed] = useState(false);
   const end = Array.isArray(event.date) ? event.date[1] : event.date;
 
   return (
-    <>
-      <Meta
-        title={event.title}
-        headerTitle="Events"
-        image={`/assets/events/${event.banner}`}
-        description={
-          metaDesc.length > 250 ? `${metaDesc.slice(0, 250)}...` : metaDesc
-        }
-        headerDescription={`Event Timers for ${event.shortTitle}`}
-      />
-      <Clocks />
-      <EventHero banner={event.banner} title={event.shortTitle} />
-      <EventInfoSection
-        title={event.title}
-        date={event.date}
-        description={event.description}
-        requires={event.requires}
-        modalCallback={() => setShowEmbed(true)}
-      />
-
+    <EventPageLayout event={event}>
       {(event.schedules || event.times) && (
         <CardGrid className={styles.cardgrid}>
           {event.times && (
@@ -96,13 +70,6 @@ export default function EventPage({ event, servants, ces }: EventPageProps) {
             Show related Upgrades
           </ActionButton>
         ))}
-
-      {showEmbed && (
-        <EventNewsModal
-          url={event.url}
-          closeCallback={() => setShowEmbed(false)}
-        />
-      )}
-    </>
+    </EventPageLayout>
   );
 }
---
+++ src/pages/EventPage/components/EventPageLayout.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Clocks from "src/client/components/Clocks";
+import Meta from "src/client/components/Meta";
+import { RequiredChildren } from "src/types/ComponentProps";
+import type { EventPageProps } from "../getStaticProps";
+import { EventHero } from "./EventHero";
+import { EventInfoSection } from "./EventInfoSection";
+import { EventNewsModal } from "./EventNewsModal";
+
+interface LayoutProps extends Pick<EventPageProps, "event"> {
+  children: RequiredChildren;
+  description?: string;
+}
+
+export function EventPageLayout({ children, event, description }: LayoutProps) {
+  const [showEmbed, setShowEmbed] = useState(false);
+  const metaDesc = description || event.description.split("\n")[0];
+
+  return (
+    <>
+      <Meta
+        title={event.title}
+        headerTitle="Events"
+        image={`/assets/events/${event.banner}`}
+        description={
+          metaDesc.length > 250 ? `${metaDesc.slice(0, 250)}...` : metaDesc
+        }
+        headerDescription={`Event Timers for ${event.shortTitle}`}
+      />
+      <EventHero banner={event.banner} title={event.shortTitle} />
+      <EventInfoSection
+        title={event.title}
+        date={event.date}
+        description={event.description}
+        requires={event.requires}
+        modalCallback={() => setShowEmbed(true)}
+      />
+      {children}
+      {showEmbed && (
+        <EventNewsModal
+          url={event.url}
+          closeCallback={() => setShowEmbed(false)}
+        />
+      )}
+    </>
+  );
+}
