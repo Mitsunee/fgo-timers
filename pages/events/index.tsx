3a4e67a6ee6dbae6d93b4a2bb82123c675c7ed66
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-02-08 12:32:33 +0100

    fix: pagination not applied in EventsPage

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -14,17 +14,19 @@ const noOp = {};
 
 export default function EventsPage({ fallback }: EventsPageProps) {
   const { perPage } = useStore(settingsStore);
-  const {
-    data = fallback,
-    isLoading,
-    isError
-  } = api.events.basic.useQuery(noOp, {
+  const query = api.events.basic.useQuery(noOp, {
     placeholderData: fallback,
     refetchOnReconnect: false,
     refetchOnWindowFocus: false
   });
   const [page, setPage] = useState<number>(1);
-  const maxPage = Math.ceil(data.length / perPage);
+
+  const isLoading = query.isLoading || !query.data;
+  const isError = query.isError;
+  const events = query.data ?? fallback;
+  const maxPage = Math.ceil(events.length / perPage);
+  const lastItemNum = Math.min(page * perPage, events.length);
+  const eventsShown = events.slice(0, lastItemNum);
 
   const handleShowMore = () => {
     setPage(page =>
@@ -51,29 +53,20 @@ export default function EventsPage({ fallback }: EventsPageProps) {
         description="List of current and past events of Fate/Grand Order"
       />
       <p>
-        Note: I currently only have 4 events in the data, so pagination cannot
-        be tested right now... oops.
+        Note: Still need to update 2023 events to include all the new details
       </p>
       {/* DEBUG */}
       <Headline>Debug</Headline>
       <p>
-        Results 1 to {Math.min(page * perPage, data.length)} of {data.length}
+        Results 1 to {lastItemNum} of {events.length}
       </p>
       <code>
         <pre>
-          {JSON.stringify(
-            {
-              isLoading,
-              isError,
-              data
-            },
-            null,
-            2
-          )}
+          {JSON.stringify({ isLoading, isError, events: eventsShown }, null, 2)}
         </pre>
       </code>
       <p>
-        Results 1 to {Math.min(page * perPage, data.length)} of {data.length}
+        Results 1 to {lastItemNum} of {events.length}
       </p>
       {!isLoading && page < maxPage && (
         <Scroller
