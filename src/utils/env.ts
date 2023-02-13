a7fcd446f05888344aa3e82fc6ac4cf964abf9e8
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-02-13 19:05:27 +0100

    fix: hostUrl missing domain in local dev

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ export const hostUrl = (() => {
     process.env.NEXT_PUBLIC_DOMAIN ??
     process.env.NEXT_PUBLIC_VERCEL_URL ??
     "http://localhost:3000";
-  return domain.startsWith("http") ? "" : `https://${domain}`;
+  return domain.startsWith("http") ? domain : `https://${domain}`;
 })();
 
 export function getHostUrl() {
