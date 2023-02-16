bd9e7b63d6cfd7ce3bbf7ee55a348dfc075838f5
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-02-16 17:50:26 +0100

    add: @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
-import type { AppType } from "next/app";
 import "modern-normalize/modern-normalize.css";
+import type { AppType } from "next/app";
+import { Dosis, Noto_Sans } from "@next/font/google";
 import "src/client/styles/app.css";
 import { Layout } from "src/client/components/Layout";
 import { api } from "src/client/api";
 
+const dosis = Dosis({
+  weight: ["400", "700"],
+  style: "normal",
+  subsets: ["latin"],
+  display: "swap"
+});
+
+const noto = Noto_Sans({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap"
+});
+
 const App: AppType = ({ Component, pageProps }) => {
   return (
     <Layout>
+      <style jsx global>
+        {`
+          :root {
+            --sans: ${noto.style.fontFamily};
+            --title: ${dosis.style.fontFamily};
+          }
+        `}
+      </style>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,16 +19,6 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="manifest" href="/manifest.webmanifest" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;700&family=Noto+Sans&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
