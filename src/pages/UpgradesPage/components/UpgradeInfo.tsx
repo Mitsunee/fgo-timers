20bb83886a4ac9fcc7795073d8b995b6cc9a3b3e
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-02-22 12:57:54 +0100

    change: use estimate prop and date comparison in UpgradeInfo

--- src/pages/UpgradesPage/components/UpgradeInfo.tsx
+++ src/pages/UpgradesPage/components/UpgradeInfo.tsx
@@ -1,3 +1,6 @@
+import { useStore } from "@nanostores/react";
+import { intervalStore } from "src/client/stores/intervalStore";
+import { useIsClient } from "src/client/utils/hooks/useIsClient";
 import { AtlasLink } from "src/client/components/AtlasLink";
 import {
   DisplayDate,
@@ -19,7 +22,10 @@ export function UpgradeInfo({
   servant,
   servantId
 }: UpgradeInfoProps) {
+  const isClient = useIsClient();
+  const { seconds: current } = useStore(intervalStore);
   const time = quest.open * 1000;
+
   return (
     <section>
       {quest.unlock && (
@@ -47,18 +53,18 @@ export function UpgradeInfo({
         </>
       )}
       <p>
-        <b>Release{quest.na ? "d" : "s"}:</b>{" "}
-        {quest.na ? (
-          <DisplayDate time={time} format="date" serverTz="always" />
-        ) : (
+        <b>Release{isClient && current >= quest.open ? "d" : "s"}:</b>{" "}
+        {quest.estimate ? (
           <DisplayDateEstimate time={time} />
+        ) : (
+          <DisplayDate time={time} format="date" serverTz="always" />
         )}
         <br />
         <AtlasLink link={`quest/${questId}/1`} na={quest.na} targetBlank>
           Quest Info on Atlas Academy DB
         </AtlasLink>
         <br />
-        <AtlasLink link={`servant/${servantId}`} na={servant.na}>
+        <AtlasLink link={`servant/${servantId}`} na={servant.na} targetBlank>
           Servant Info on Atlas Academy DB
         </AtlasLink>
       </p>
