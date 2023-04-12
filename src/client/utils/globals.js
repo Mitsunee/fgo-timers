62c6bdb1bf185a7b04ff36ff4ef9e1dd0ddba135
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-04-12 21:22:40 +0200

    remove: unused deprecated client utils

--- src/client/utils/globals.js
+++ src/client/utils/globals.js
@@ -1,11 +1,26 @@
 // TODO: move into GLOBAL enum
 
-export const MAX_AP_MIN_VALUE = 20; // NOTE: already moved as AP_MAX, this var is deprecated
-export const MAX_AP_MAX_VALUE = 144; // NOTE: already moved as AP_MAX, this var is deprecated
+/**
+ * @deprecated
+ */
+export const MAX_AP_MIN_VALUE = 20;
+/**
+ * @deprecated
+ */
+export const MAX_AP_MAX_VALUE = 144;
 export const NODE_COST_MIN_VALUE = 3;
 export const NODE_COST_MAX_VALUE = 40;
 export const WEEKLY_MM_LEN = 604800;
 export const WEEKLY_MM_OFFSET = 345600;
+/**
+ * @deprecated
+ */
 export const SERVER_DAY_MS_LEN = 8640000;
+/**
+ * @deprecated
+ */
 export const SERVER_DAY_MS_OFFSET = 3643200;
+/**
+ * @deprecated
+ */
 export const SERVER_DAY_ZERO = 1498449600;
