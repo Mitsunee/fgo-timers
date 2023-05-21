60b9de0a101f6275fce72971153c8ed88350c9a1
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-05-21 19:05:44 +0200

    remove: old check script + utils

--- src/scripts/utils/data-assets/parseDataFile.mjs
+++
@@ -1,8 +0,0 @@
-import { findLegacyParser } from "./findLegacyParser.mjs";
-
-export async function parseDataFile(filePath) {
-  const [fileType, parser] = findLegacyParser(filePath);
-  if (!parser) throw new Error("Could not find parser");
-  const data = await parser(filePath);
-  return { type: fileType, data };
-}
