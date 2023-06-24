70b472c6a70431aa4fee58cd82228fd9252900f7
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-06-24 16:21:53 +0200

    fix: migrate servants/getOwner and upgrades/getRelated to new cache utils

--- src/servants/getOwner.ts
+++ src/servants/getOwner.ts
@@ -1,7 +1,7 @@
 import type { NoblePhantasm } from "@atlasacademy/api-connector/dist/Schema/NoblePhantasm";
 import type { Servant } from "@atlasacademy/api-connector/dist/Schema/Servant";
 import type { Skill } from "@atlasacademy/api-connector/dist/Schema/Skill";
-import { atlasCache } from "~/atlas-api/cache";
+import { getNiceServantsFull } from "~/atlas-api/cache/data/niceServant";
 import type { SupportedRegion } from "~/atlas-api/api";
 
 /**
@@ -14,7 +14,7 @@ export async function getNPOwner(
   subject: NoblePhantasm,
   region: SupportedRegion = "JP"
 ): Promise<Servant | undefined> {
-  const niceServant = await atlasCache[region].getNiceServant();
+  const niceServant = await getNiceServantsFull(region);
 
   return niceServant.find(servant =>
     servant.noblePhantasms.some(np => np.id == subject.id)
@@ -31,7 +31,7 @@ export async function getSkillOwners(
   subject: Skill,
   region: SupportedRegion = "JP"
 ): Promise<Servant[]> {
-  const niceServant = await atlasCache[region].getNiceServant();
+  const niceServant = await getNiceServantsFull(region);
 
   return niceServant.filter(servant =>
     servant.skills.some(skill => skill.id == subject.id)
