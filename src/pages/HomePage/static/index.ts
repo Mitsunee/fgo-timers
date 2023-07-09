fe31f3d22f77aca5b4f33c799803fc0480569f42
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-07-09 13:54:36 +0200

    refactor: migrate HomePage to new utils

--- src/pages/HomePage/static/index.ts
+++ src/pages/HomePage/static/index.ts
@@ -1,30 +1,24 @@
 import type { GetStaticPropsResult, InferGetStaticPropsType } from "next";
 import { serverApi } from "~/server/api/root";
 import * as Legacy from "~/server/HomePage";
+import { createItemRecord } from "~/static/data/items";
 import { msToSeconds } from "~/time/msToSeconds";
-import { getBundledItemMap } from "~/utils/getBundles";
 import { getLoginTicketProps } from "./getLoginTicketProps";
 import { getMilestoneProps } from "./getMilestoneProps";
 import { getShopInfoProps } from "./getShopInfoProps";
 
 export const getStaticProps = async () => {
   const now = msToSeconds(Date.now());
-  const [legacyProps, itemMap, events, loginTicket, milestones, shops] =
+  const [legacyProps, events, loginTicket, milestones, shops] =
     await Promise.all([
       Legacy.getStaticProps(),
-      getBundledItemMap(),
       serverApi.events.basic.fetch({ exclude: "inactive", now }),
       getLoginTicketProps(now),
       getMilestoneProps(now),
       getShopInfoProps(now)
     ]);
 
-  const itemIds = new Set<number>([...loginTicket.items]);
-  const items: typeof itemMap = {};
-
-  for (const id of itemIds) {
-    items[id] = itemMap[id];
-  }
+  const items = await createItemRecord(new Set<number>(loginTicket.items));
 
   /* const special = {
     title: "Next Chaldea Broadcasting Station",
